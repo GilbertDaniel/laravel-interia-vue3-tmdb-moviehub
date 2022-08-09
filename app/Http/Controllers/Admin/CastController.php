d2eab25d5d1c77ec55f0bc4bcb197749be12450b
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-08-09 18:54:54 +0630

    CRUD for Casts

--- app/Http/Controllers/Admin/CastController.php
+++ app/Http/Controllers/Admin/CastController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Cast;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Str;
 use Inertia\Inertia;
 
 class CastController extends Controller
@@ -15,7 +19,17 @@ class CastController extends Controller
      */
     public function index()
     {
-        return Inertia::render('Casts/Index');
+        $perPage = Request::input('perPage') ?: 5;
+
+        return Inertia::render('Casts/Index', [
+            'casts' => Cast::query()
+                ->when(Request::input('search'), function ($query, $search) {
+                    $query->where('name', 'like', "%{$search}%");
+                })
+                ->paginate($perPage)
+                ->withQueryString(),
+            'filters' => Request::only(['search', 'perPage'])
+        ]);
     }
 
     /**
@@ -36,7 +50,25 @@ class CastController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $cast = Cast::where('tmdb_id', Request::input('castTMDBId'))->first();
+        if ($cast) {
+            return Redirect::back()->with('flash.banner', 'Cast Exists.');
+        }
+
+        // get cast information from TMDB by cast id
+        $tmdb_cast = Http::asJson()->get(config('services.tmdb.endpoint').'person/' . Request::input('castTMDBId') . '?api_key='. config('services.tmdb.secret') .'&language=en-US');
+
+        if ($tmdb_cast->successful()) {
+            Cast::create([
+                'tmdb_id' => $tmdb_cast['id'],
+                'name'    => $tmdb_cast['name'],
+                'slug'    => Str::slug($tmdb_cast['name']),
+                'poster_path' => $tmdb_cast['profile_path']
+            ]);
+            return Redirect::back()->with('flash.banner', 'Cast created.');
+        } else {
+            return Redirect::back()->with('flash.banner', 'Api error.');
+        }
     }
 
     /**
@@ -56,9 +88,9 @@ class CastController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Cast $cast)
     {
-        //
+        return Inertia::render('Casts/Edit', ['cast' => $cast]);
     }
 
     /**
@@ -68,9 +100,14 @@ class CastController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Cast $cast)
     {
-        //
+        $validated = Request::validate([
+            'name' => 'required',
+            'poster_path' => 'required'
+        ]);
+        $cast->update($validated);
+        return Redirect::route('admin.casts.index')->with('flash.banner', 'Cast Updated.');
     }
 
     /**
@@ -79,8 +116,9 @@ class CastController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Cast $cast)
     {
-        //
+        $cast->delete();
+        return Redirect::back()->with('flash.banner', 'Cast Deleted.')->with('flash.bannerStyle', 'danger');
     }
 }
