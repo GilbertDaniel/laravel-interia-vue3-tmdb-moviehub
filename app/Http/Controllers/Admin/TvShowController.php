452ef76f7e11195c594010a9aedefec721e0d564
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-08-09 19:25:17 +0630

    CRUD for TvShow

--- app/Http/Controllers/Admin/TvShowController.php
+++ app/Http/Controllers/Admin/TvShowController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\TvShow;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Request;
 use Inertia\Inertia;
+use Illuminate\Support\Str;
 
 class TvShowController extends Controller
 {
@@ -15,7 +19,17 @@ class TvShowController extends Controller
      */
     public function index()
     {
-        return Inertia::render('TvShows/Index');
+        $perPage = Request::input('perPage') ?: 5;
+
+        return Inertia::render('TvShows/Index', [
+            'tvShows' => TvShow::query()
+                ->when(Request::input('search'), function ($query, $search) {
+                    $query->where('name', 'like', "%{$search}%");
+                })
+                ->paginate($perPage)
+                ->withQueryString(),
+            'filters' => Request::only(['search', 'perPage'])
+        ]);
     }
 
     /**
@@ -34,9 +48,26 @@ class TvShowController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store()
     {
-        //
+        $tvShow = TvShow::where('tmdb_id', Request::input('tvShowTMDBId'))->first();
+        if ($tvShow) {
+            return Redirect::back()->with('flash.banner', 'Tv Show Exists.');
+        }
+
+        $tmdb_tv = Http::asJson()->get(config('services.tmdb.endpoint') . 'tv/' . Request::input('tvShowTMDBId') . '?api_key=' . config('services.tmdb.secret') . '&language=en-US');
+        if ($tmdb_tv->successful()) {
+            TvShow::create([
+                'tmdb_id' => $tmdb_tv['id'],
+                'name'    => $tmdb_tv['name'],
+                'slug' => Str::slug($tmdb_tv['name']),
+                'poster_path' => $tmdb_tv['poster_path'],
+                'created_year' => $tmdb_tv['first_air_date']
+            ]);
+            return Redirect::back()->with('flash.banner', 'Tv Show created.');
+        } else {
+            return Redirect::back()->with('flash.banner', 'Api error.');
+        }
     }
 
     /**
@@ -56,9 +87,9 @@ class TvShowController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(TvShow $tvShow)
     {
-        //
+        return Inertia::render('TvShows/Edit', ['tvShow' => $tvShow]);
     }
 
     /**
@@ -68,9 +99,14 @@ class TvShowController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(TvShow $tvShow)
     {
-        //
+        $validated = Request::validate([
+            'name'    => 'required',
+            'poster_path' => 'required'
+        ]);
+        $tvShow->update($validated);
+        return Redirect::route('admin.tv-shows.index')->with('flash.banner', 'Tv Show updated.');
     }
 
     /**
@@ -79,8 +115,9 @@ class TvShowController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(TvShow $tvShow)
     {
-        //
+        $tvShow->delete();
+        return Redirect::route('admin.tv-shows.index')->with('flash.banner', 'Tv Show deleted.')->with('flash.bannerStyle', 'danger');
     }
 }
